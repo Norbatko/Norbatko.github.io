aee6836439060e449cb1fc69afcd5d92be09dde4
Tomáš Ebert <98109213+Norbatko@users.noreply.github.com>
2023-04-23 22:19:04 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const text = document.querySelector(".second-text");
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Tomáš Ebert.";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "a programmer.";
+    }, 4000);
+}
+
+textLoad();
+setInterval(textLoad, 8000);
+
