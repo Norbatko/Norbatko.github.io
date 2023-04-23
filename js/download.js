6ab0580341cdb0e3361658a33f413b6092de1429
Tomáš Ebert <98109213+Norbatko@users.noreply.github.com>
2023-04-23 23:37:29 +0200

    Comments

--- js/download.js
+++ js/download.js
@@ -1,4 +1,6 @@
 function download_sound() {
+    // Spusteni zvuku buceni kravy     
     document.getElementById('kow_play').play();
+    // Stazeni instalacniho balicku
     window.open('/download/kowculator_1.0_amd64.deb');
 }
